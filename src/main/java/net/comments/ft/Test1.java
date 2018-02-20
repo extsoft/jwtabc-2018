3fababffa0e791b848d5c3ca916497fdd482e47d
Dmytro Serdiuk <dmytro.serdiuk@gmail.com>
2018-02-20 10:46:38 +0200

    Simplify work with WebDriver

---
+++ src/main/java/net/comments/ft/Test1.java
@@ -0,0 +1,27 @@
+package net.comments.ft;
+
+import net.comments.objects.BCommentPage;
+import net.comments.objects.CommentPage;
+import org.hamcrest.MatcherAssert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+public class Test1 extends BaseDriver {
+
+    @Test
+    public void test() {
+        CommentPage commentPage = new BCommentPage(this.driver);
+        commentPage.open();
+        commentPage.newComment();
+        this.driver.findElement(By.xpath("//*[@id=\"Text\"]")).clear();
+        this.driver.findElement(By.xpath("//*[@id=\"Text\"]")).sendKeys("Newly created comment 987");
+        this.driver.findElements(By.id("Categories")).get(0).click();
+        this.driver.findElements(By.id("Categories")).get(4).click();
+        this.driver.findElement(By.xpath("//*[@id=\"categoryselector\"]/div[2]/input[2]")).click();
+        this.driver.findElement(By.xpath("//*[@id=\"editor-navigation\"]/input[2]")).click();
+        this.driver.findElement(By.xpath("//*[@id=\"main\"]/div/div[5]/form/table/tfoot/tr/td/a[3]")).click();
+
+        final String page = this.driver.getPageSource();
+        MatcherAssert.assertThat("New comment is present", page.contains("Newly created comment 987"));
+    }
+}
---
+++ src/main/java/net/comments/ft/Test4.java
@@ -0,0 +1,26 @@
+package net.comments.ft;
+
+import net.comments.objects.BCommentPage;
+import net.comments.objects.CommentPage;
+import org.hamcrest.MatcherAssert;
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+/**
+ * @author Dmytro Serdiuk (dmytro.serdiuk@gmail.com)
+ * @version $Id$
+ * @since ?????
+ */
+public class Test4 extends BaseDriver {
+
+    @Test
+    public void test() {
+        CommentPage commentPage = new BCommentPage(this.driver);
+        commentPage.open();
+        this.driver.findElements(By.name("SelectedId")).get(0).click();
+        this.driver.findElement(By.xpath("//*[@value=\"Delete\"]")).click();
+        this.driver.findElement(By.xpath("//span[text()=\"Yes\"]")).click();
+        final String page = this.driver.getPageSource();
+        MatcherAssert.assertThat("Comment Text 0 is present", !page.contains("Comment Text 0"));
+    }
+}
